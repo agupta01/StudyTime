ee619e248415102ffc9e7571f27364f005015269
Arunav Gupta <centaurus.ab@gmail.com>
2019-10-26 05:37:07 -0700

    fixed database feature

--- StudyTime/InfoTable.js
+++ StudyTime/InfoTable.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import { Platform, StyleSheet, Text, View, ActivityIndicator, FlatList } from 'react-native';
 
 var userData = require('./data.json');
 
@@ -25,41 +25,15 @@ export default class InfoTable extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {isLoading: true}
+    this.dataSource = userData.history[0].times;
   }
-
-  componentDidMount(){
-    return fetch('./data.json')
-      .then((response) => response.json())
-      .then((responseJson) => {
-
-        this.setState({
-          isLoading: false,
-          dataSource: responseJson.history[0].times,
-        }, function(){
-
-        });
-
-      })
-      .catch((error) =>{
-        console.error(error);
-      });
-  }
-
 
   render() {
-    if (this.state.isLoading) {
-      return(
-        <View style={{flex: 1, padding: 20}}>
-          <ActivityIndicator/>
-        </View>
-      )
-    }
     return (
       <View style={this.styles.container}>
         <FlatList
-          data={this.state.dataSource}
-          renderItem={({item}) => <Text style={this.styles.instructions}>{item.time}}</Text>}
+          data={this.dataSource}
+          renderItem={({item}) => <Text style={this.styles.instructions}>{item.time}</Text>}
           keyExtractor={({id}, index) => id}
         />
       </View>
